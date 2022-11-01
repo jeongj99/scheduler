e0e8933a1969c26f30dce459c2e278e18587834d
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-01 15:36:26 -0300

    Add conditional rendering depending on props to Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,9 +3,17 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
+  let buttonClass = "button";
+
+  if (props.confirm) {
+    buttonClass += " button--confirm";
+  }
+
+  if (props.danger) {
+    buttonClass += " button--danger"
+  }
+
   return (
-    <button>
-      {props.children}
-    </button>
+    <button className={buttonClass}>{props.children}</button>
   );
 }

69bab2a26eedcf78fc48f9101917f63712e5ae9b
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-01 18:05:27 -0300

    Use map to dynamically create an array of DayListItems and render it in DayList

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,8 +1,21 @@
 import React from "react";
 import DayListItem from "./DayListItem";
 
-export default function DayListItem(props) {
+export default function DayList(props) {
+
+  const items = props.days.map(day =>
+    <DayListItem
+      key={day.id}
+      name={day.name}
+      spots={day.spots}
+      selected={day.name === props.day}
+      setDay={props.setDay}
+    />
+  );
+
   return (
-    <ul></ul>
+    <ul>
+      {items}
+    </ul>
   );
 }

8930b9fbcf50b963f6570f17a2f5df8db36f538d
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-01 17:30:04 -0300

    Create DayListItem scss and add initial styling

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,7 +4,7 @@ export default function DayListItem(props) {
   const { name, spots, selected, setDay } = props;
 
   return (
-    <li selected={selected} onClick={() => setDay({ name })}>
+    <li onClick={() => setDay({ name })}>
       <h2 className="text--regular">{name}</h2>
       <h3 className="text--light">{spots} spots remaining</h3>
     </li>

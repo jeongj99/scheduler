d71da5386bcab0b5b89f9a9f4b50ebaa5eb09ba8
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-01 17:35:02 -0300

    Import classnames and use it to coditionally apply classes to the <li> in DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,17 @@
 import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
   const { name, spots, selected, setDay } = props;
 
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": selected,
+    "day-list__item--full": spots === 0
+  });
+
   return (
-    <li onClick={() => setDay({ name })}>
+    <li className={dayClass} onClick={() => setDay({ name })}>
       <h2 className="text--regular">{name}</h2>
       <h3 className="text--light">{spots} spots remaining</h3>
     </li>

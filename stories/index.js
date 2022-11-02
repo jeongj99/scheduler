3ddde694bc77ba4410bb48978fa59a57403af1a8
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-02 20:43:03 -0300

    Import Appointment to index in story and add stories for Appointment

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,7 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment/index";
 
 // Stories for Button
 storiesOf("Button", module)
@@ -137,3 +138,11 @@ storiesOf("InterviewerList", module)
       onChange={action("setInterviewer")}
     />
   ));
+
+// Stories for Appointment
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />);

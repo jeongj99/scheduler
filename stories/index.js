b0f14d0b94a965bc287b0f2d0c9ea4203ca9436a
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-02 21:05:40 -0300

    Create initial Show component and import it to index in story folder

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 // Stories for Button
 storiesOf("Button", module)

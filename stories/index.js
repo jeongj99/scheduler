1a9759358bfd2da95304d40739c8c295043fe1ce
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-02 21:02:51 -0300

    Create a story for Empty that passes a onAdd function and refactor Empty component to display that

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,7 @@ import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 // Stories for Button
 storiesOf("Button", module)
@@ -147,4 +148,5 @@ storiesOf("Appointment", module)
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
-  .add("Header", () => <Header time="12pm" />);
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />);

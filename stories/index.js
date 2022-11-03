f49f1877110b65131c9615c4acb4bb0651d18f6d
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-02 21:27:49 -0300

    Create a Status story that passes props and refactor Status so it uses those props

--- stories/index.js
+++ stories/index.js
@@ -167,4 +167,5 @@ storiesOf("Appointment", module)
       onConfirm={action("onConfirm")}
       onCancel={action("onCancel")}
     />
-  ));
+  ))
+  .add("Status", () => <Status message="Deleting" />);

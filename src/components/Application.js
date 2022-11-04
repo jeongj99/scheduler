8afbd922257d8e7e871225ac19e16a1e487152b1
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-04 12:25:50 -0300

    Update the state object to include an interviewers object and update the effect method to do a third axios request using Promise.all

--- src/components/Application.js
+++ src/components/Application.js
@@ -11,7 +11,8 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   const setDay = day => setState({ ...state, day });
@@ -19,9 +20,10 @@ export default function Application(props) {
   useEffect(() => {
     Promise.all([
       axios.get("/api/days"),
-      axios.get("api/appointments")
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
     ]).then(all => {
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data }));
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     });
   }, []);
 

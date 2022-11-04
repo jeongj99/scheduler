a2fe1beb3037e1a821949d988fe1add3b2c36ff8
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-03 21:37:21 -0300

    Create the getAppointmentsForDay Selector function and pass all the unit tests

---
+++ src/helpers/selectors.js
@@ -0,0 +1,17 @@
+export function getAppointmentsForDay(state, day) {
+
+  const result = [];
+
+  const selectedDay = (state.days).filter(stateDay => stateDay.name === day);
+
+  if (state.days.length === 0 || selectedDay.length === 0) {
+    return [];
+  }
+
+  for (const appointment of selectedDay[0].appointments) {
+    if (state.appointments[appointment]) {
+      result.push(state.appointments[appointment]);
+    }
+  }
+  return result;
+}

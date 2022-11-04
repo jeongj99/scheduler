8d0492d97f2ae221859a962eacff7b715a0ef815
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-03 22:17:42 -0300

    Update the effect to set the days and appointments state at the same time

--- src/components/Application.js
+++ src/components/Application.js
@@ -61,7 +61,7 @@ export default function Application(props) {
       axios.get("/api/days"),
       axios.get("api/appointments")
     ]).then(all => {
-      console.log(all);
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data }));
     });
   }, []);
 

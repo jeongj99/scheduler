e3bc9af9b04e1a71d7e9e68c9450abc37c68359d
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-03 22:02:07 -0300

    Delete previous states and add a state that its default is an object containing the previous states

--- src/components/Application.js
+++ src/components/Application.js
@@ -46,8 +46,14 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState({ ...state, days });
 
   useEffect(() => {
     axios.get("/api/days").then(response => {
@@ -66,8 +72,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>

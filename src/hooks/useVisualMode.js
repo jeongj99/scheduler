9e05d699331c524549e4d14e0a0ffa61b982cc4f
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-04 20:35:18 -0300

    Implement back function and update the states so only history state is present and mode is just dependent on history state

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,15 +1,15 @@
 import { useState } from "react";
 
 export default function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
+  let mode = history[history.length - 1];
 
   const transition = (newMode) => {
-    setMode(newMode);
+    setHistory(prev => [...prev, newMode]);
   };
 
   const back = () => {
-
+    setHistory(prev => prev.slice(0, prev.length - 1));
   };
 
   return { mode, transition, back };

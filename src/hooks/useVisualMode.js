a433c9136d4974efa6031efefe90ad1d3738e7f3
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-04 21:04:16 -0300

    Add a test for transition with replace, and update transition function to pass the test

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,15 +4,13 @@ export default function useVisualMode(initial) {
   const [history, setHistory] = useState([initial]);
   let mode = history[history.length - 1];
 
-  const transition = (newMode) => {
-    setHistory(prev => [...prev, newMode]);
+  const transition = (newMode, replace = false) => {
+    setHistory(prev => replace ? [...(prev.slice(0, prev.length - 1)), newMode] : [...prev, newMode]);
   };
 
   const back = () => {
     setHistory(prev => prev.length === 1 ? prev : prev.slice(0, prev.length - 1));
   };
-  console.log(history);
-  console.log(mode);
 
   return { mode, transition, back };
 }

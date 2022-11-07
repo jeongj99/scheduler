ca9a0f8c2270669f7cc3f53e546a3dba36ae50bc
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-07 20:55:54 -0300

    Add a transition to SAVING before calling bookInterview and import the Status component so that it is displayed when mode is = to SAVING

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,6 +6,7 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
+import Status from "./Status";
 
 import useVisualMode from "hooks/useVisualMode";
 
@@ -15,6 +16,7 @@ export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const SAVING = "SAVING";
 
   const { mode, transition, back } = useVisualMode(
     interview ? SHOW : EMPTY
@@ -25,6 +27,8 @@ export default function Appointment(props) {
       student: name,
       interviewer
     };
+
+    transition(SAVING);
 
     bookInterview(id, interview)
       .then(() => {
@@ -46,6 +50,7 @@ export default function Appointment(props) {
         />
       )}
       {mode === CREATE && <Form interviewers={interviewers} onCancel={back} onSave={save} />}
+      {mode === SAVING && <Status message="Saving" />}
     </article>
   );
 }

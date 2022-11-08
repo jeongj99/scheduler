6dcd1b6302093adc3367577216740f4d667c03f6
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-07 22:37:47 -0300

    Add replace as true in transition to ERROR_DELETE and ERROR_SAVE

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -42,7 +42,7 @@ export default function Appointment(props) {
         transition(SHOW);
       })
       .catch(() => {
-        transition(ERROR_SAVE);
+        transition(ERROR_SAVE, true);
       });
   };
 
@@ -54,7 +54,7 @@ export default function Appointment(props) {
         transition(EMPTY);
       })
       .catch(() => {
-        transition(ERROR_DELETE);
+        transition(ERROR_DELETE, true);
       });
   };
 

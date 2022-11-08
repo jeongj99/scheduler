a1ff418b29b3f92951b564e0b5b04856cdd70f1c
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-11-07 22:38:39 -0300

    Add replace as true in transition to DELETE

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -47,7 +47,7 @@ export default function Appointment(props) {
   };
 
   const deleting = () => {
-    transition(DELETING);
+    transition(DELETING, true);
 
     cancelInterview(id)
       .then(() => {
